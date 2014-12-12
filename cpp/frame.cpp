a4c2810f0898e7adcb6079917788cbf77e3c08ed
Villedieu De Torcy Antoine <villedieudet_ant@sirtl8.metz.supelec.fr>
2014-12-12 12:18:42 +0100

    fin correction

--- cpp/frame.cpp
+++ cpp/frame.cpp
@@ -18,41 +18,49 @@
 	void Frame::set_lpc(int ordre_lpc){
 
 		lpc = new double[ordre_lpc]; 
+		double lpc_up[ordre_lpc]; 
 		double autocorr[taille - ordre_lpc];
 
-		std::cout << "taille : " << taille;
-		for(int i = ordre_lpc+1; i < taille; i++){
-			autocorr[i-(ordre_lpc+1)] = 0;
-			for(int j = 0; j <= i ; j++){
-				if (i == 25)
-				std::cout << "signal decale : " << signal[taille +j-i-1] << std::endl;
-				autocorr[i-(ordre_lpc+1)] += signal[j] * signal[taille +j-i-1];
+		for(int i = 0; i < ordre_lpc; i++){
+			autocorr[i] = 0;
+			for(int j = 0; j <= taille-1-i ; j++){
+				autocorr[i] += signal[j] * signal[j + i];
 			}
   		}
+  		///////////// test en cours
+		std::cout << "Contenu de autocorr : " << autocorr << std::endl;
 
-
-		///////////// test en cours
-		std::cout << "Contenu de autocorr : " << autocorr;
 
 		double tmp = -autocorr[1]/autocorr[0];
 		lpc[0]=(tmp);
 		double sigma2 = (1 - tmp*tmp) * autocorr[0];
 
-		for(int i = 1; i < ordre_lpc; i++){
-			tmp = autocorr[i+1];
-			for (int k = 1; k < i; ++k){
-				tmp += lpc[k-1]*autocorr[i-k];
+		for(int k = 2; k< ordre_lpc; k++){
+			tmp = autocorr[k];
+
+			for (int j = 1; j < k; ++j){
+				tmp += lpc[j-1]*autocorr[k-j];
 			}
 			tmp /= -sigma2;
 
-			for (int k = 0; k < i-1; ++k){
-				lpc[k] += tmp*lpc[i-k-1];
+			//std::cout << "tmp = " << tmp << std::endl;
+
+			for (int j = 1; j < k; ++j){
+				if (k==3){
+					std::cout << "j = " << j << std::endl;
+					std::cout << "tmp = " << tmp << std::endl;
+					std::cout << "lpc[j-1] = " << lpc[j-1] << std::endl;
+					std::cout << "lpc[k-1-j] = " << lpc[k-1-j] << std::endl;
+				}
+				lpc_up[j-1] = lpc[j-1] + tmp*lpc[k-1-j];
 			}
-			lpc[i-1] = tmp;
+			lpc = lpc_up; //attention recopie, faire une boucle ?
+			lpc[k-1] = tmp;
 
         	sigma2 *=  (1 - tmp*tmp);
 		}
-
+		///////////// test en cours
+		std::cout << "Contenu de lpc : " << lpc << std::endl;
 	}
 
 	void Frame::set_mfcc(int nb_mfcc){
